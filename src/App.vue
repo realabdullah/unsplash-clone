f09be0e2bdd021ddc660abf71a8ac5923a91b76d
realabdullah <abdulodesanmi@gmail.com>
2022-01-16 22:21:34 +0100

    searchbar

--- src/App.vue
+++ src/App.vue
@@ -3,4 +3,21 @@
 </template>
 
 <style>
+:root {
+  --gray: #a7aebc;
+  --lightgray: #dde2e9;
+  --darkgray: #999393;
+  --lavender: #e7e9ed;
+  --white: #ffffff;
+}
+
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+ul, ol, li {
+  list-style: none;
+}
 </style>
---
+++ src/components/Search.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="search-bar">
+    <input type="search" name="search" placeholder="Search for a photo">
+    <i class="fas fa-search"></i>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+  .search-bar {
+    background: var(--lightgray);
+    width: 100%;
+    padding-block: 3rem;
+    padding-inline: 5rem;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    /* position: relative; */
+  }
+
+  .search-bar input {
+    width: inherit;
+    border-style: none;
+    padding: 10px;
+    outline: none;
+    border-radius: 5px;
+    background-color: var(--white);
+    color: var(--darkgray);
+  }
+
+  .search-bar input::placeholder {
+    color: var(--darkgray);
+  }
+</style>
