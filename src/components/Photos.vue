3bce2bada5e2cab16e6bdbd884c5e2f0b8dc4dc2
realabdullah <abdulodesanmi@gmail.com>
2022-01-16 23:19:13 +0100

    images componet

---
+++ src/components/Photos.vue
@@ -0,0 +1,92 @@
+<template>
+  <div class="photos-container">
+    <ul>
+      <li class="card">
+        <div class="image">
+          <img src="https://cdn.pixabay.com/photo/2021/12/21/03/54/street-6884533_960_720.jpg" alt="img">
+          <div class="author">
+            <p>John Doe</p>
+            <span>Lagos, Nigeria</span>
+          </div>
+        </div>
+      </li>
+      <li class="card">
+        <div class="image">
+          <img src="https://cdn.pixabay.com/photo/2021/12/21/03/54/street-6884533_960_720.jpg" alt="img">
+          <div class="author">
+            <p>John Doe</p>
+            <span>Lagos, Nigeria</span>
+          </div>
+        </div>
+      </li>
+      <li class="card">
+        <div class="image">
+          <img src="https://cdn.pixabay.com/photo/2021/12/21/03/54/street-6884533_960_720.jpg" alt="img">
+          <div class="author">
+            <p>John Doe</p>
+            <span>Lagos, Nigeria</span>
+          </div>
+        </div>
+      </li>
+      <li class="card">
+        <div class="image">
+          <img src="https://cdn.pixabay.com/photo/2021/12/21/03/54/street-6884533_960_720.jpg" alt="img">
+          <div class="author">
+            <p>John Doe</p>
+            <span>Lagos, Nigeria</span>
+          </div>
+        </div>
+      </li>
+      <li class="card">
+        <div class="image">
+          <img src="https://cdn.pixabay.com/photo/2021/12/21/03/54/street-6884533_960_720.jpg" alt="img">
+          <div class="author">
+            <p>John Doe</p>
+            <span>Lagos, Nigeria</span>
+          </div>
+        </div>
+      </li>
+      <li class="card">
+        <div class="image">
+          <img src="https://cdn.pixabay.com/photo/2021/12/21/03/54/street-6884533_960_720.jpg" alt="img">
+          <div class="author">
+            <p>John Doe</p>
+            <span>Lagos, Nigeria</span>
+          </div>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+.photos-container {
+  position: absolute;
+  top: 140px;
+}
+
+.photos-container ul {
+  display: flex;
+  flex-wrap: wrap;
+  align-items: center;
+  justify-content: center;
+  margin: 15px;
+}
+
+.photos-container .card .image {
+  width: 200px;
+  margin: 10px;
+  position: relative;
+}
+
+.photos-container .card img {
+  width: 100%;
+  border-radius: 7px;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,19 +1,22 @@
 <template>
   <div class="home">
     <Search />
-    <PhotosLoader />
+    <!-- <PhotosLoader /> -->
+    <Photos />
   </div>
 </template>
 
 <script>
 import Search from '@/components/Search.vue'
 import PhotosLoader from '@/components/PhotosLoader.vue'
+import Photos from '@/components/Photos.vue'
 
 export default {
   name: 'Home',
   components: {
     Search,
-    PhotosLoader
+    PhotosLoader,
+    Photos
   }
 }
 </script>
